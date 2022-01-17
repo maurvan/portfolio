67fe18e36e77d10aa8e854e2f04ec5ce25320db3
Maureen Vanhoren <maureenvanhoren@gmail.com>
2022-01-17 15:09:01 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WaveComponent } from './wave/wave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: WaveComponent },
+  { path: '', component: AboutComponent },
+  { path: '', component: SkillsComponent },
+  { path: '', component: ProjectsComponent },
+  { path: '', component: ContactComponent },
+  { path: '', component: FooterComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
