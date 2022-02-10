f08780525264546c3c6716c0cb5e7896901d23a7
Maureen Vanhoren <maureenvanhoren@gmail.com>
2022-02-10 18:29:03 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+
+import * as AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+AOS.init();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class AppComponent {
   title = 'Maureen Vanhoren';
